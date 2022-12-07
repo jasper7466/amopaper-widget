4ed2d7042269f669a2ea1ec140064e304982f0fa
Tim <jasper7466@yandex.ru>
2022-12-07 13:27:24 +0500

    Refactor. Modal template

--- src/app/components/organisms/addressee-selector/addressee-selector.component.ts
+++ src/app/components/organisms/addressee-selector/addressee-selector.component.ts
@@ -5,7 +5,7 @@ import {
   isAddresseeAddedSelector,
 } from '../../../store/addressee/selectors';
 import { resetAddresseeAction } from '../../../store/addressee/actions';
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, EventEmitter } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ModalComponent } from '../../templates/modal/modal.component';
 
@@ -16,14 +16,26 @@ import { ModalComponent } from '../../templates/modal/modal.component';
 })
 export class AddresseeSelectorComponent {
   @ViewChild(ModalComponent) modal: ModalComponent | undefined;
-  isAddresseeAdded$ = this.store.select(isAddresseeAddedSelector);
-  addresseeName$ = this.store.select(addresseeNameSelector);
-  addresseePhone$ = this.store.select(addresseePhoneSelector);
-  addresseeVatId$ = this.store.select(addresseeVatIdSelector);
+
+  protected openModalEmitter: EventEmitter<void> = new EventEmitter<void>();
+  protected closeModalEmitter: EventEmitter<void> = new EventEmitter<void>();
+
+  protected isAddresseeAdded$ = this.store.select(isAddresseeAddedSelector);
+  protected addresseeName$ = this.store.select(addresseeNameSelector);
+  protected addresseePhone$ = this.store.select(addresseePhoneSelector);
+  protected addresseeVatId$ = this.store.select(addresseeVatIdSelector);
 
   constructor(private store: Store) {}
 
-  public removeAddresseeHandler(): void {
+  public removeAddressee(): void {
     this.store.dispatch(resetAddresseeAction());
+  }
+
+  public showSelectorModal(): void {
+    this.openModalEmitter.emit();
+  }
+
+  public hideSelectorModal(): void {
+    this.closeModalEmitter.emit();
   }
 }
--- src/app/components/templates/modal/modal.component.ts
+++ src/app/components/templates/modal/modal.component.ts
@@ -1,22 +1,51 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  Input,
+  EventEmitter,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @HostBinding('class.opened') isOpened: boolean = false;
+  @Input() isClosable: boolean = true;
+  @Input() openTrigger: EventEmitter<void>;
+  @Input() closeTrigger: EventEmitter<void>;
+
+  private onInitSubscriptions: Subscription[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.onInitSubscriptions.push(
+      this.openTrigger.subscribe(() => this.open())
+    );
 
-  close(): void {
-    this.isOpened = false;
+    this.onInitSubscriptions.push(
+      this.closeTrigger.subscribe(() => this.close())
+    );
   }
 
-  open(): void {
+  ngOnDestroy(): void {
+    this.onInitSubscriptions.forEach((subscription) =>
+      subscription.unsubscribe()
+    );
+  }
+
+  protected close(): void {
+    if (this.isClosable) {
+      this.isOpened = false;
+    }
+  }
+
+  protected open(): void {
     this.isOpened = true;
   }
 }
