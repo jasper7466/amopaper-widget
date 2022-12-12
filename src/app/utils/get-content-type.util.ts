a77cd02ea7fb4204609f54ff3b7d646a52581e4b
Tim <jasper7466@yandex.ru>
2022-12-13 02:42:22 +0500

    Utils. GetContentType. Получение MIME-типа файла по его расширению

---
+++ src/app/utils/get-content-type.util.ts
@@ -0,0 +1,33 @@
+const contentTypes: { [key: string]: string } = {
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  png: 'image/png',
+  gif: 'image/gif',
+  tif: 'image/tiff',
+  tiff: 'image/tiff',
+  bmp: 'image/bmp',
+  pdf: 'application/pdf',
+  doc: 'application/msword',
+  docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  rtf: 'application/rtf',
+  xls: 'application/vnd.ms-excel',
+  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  rar: 'application/vnd.rar',
+  zip: 'application/zip',
+};
+
+/**
+ * Возвращает MIME-type соответствующий расширению или выбрасывает исключение, если
+ * тип для данного расширения отсутствует в словаре
+ * @param extOrFullName полное имя файла или его расширение ('name.ext', '.ext', 'ext')
+ * @returns
+ */
+export const getContentType = (extOrFullName: string): string | never => {
+  let ext = extOrFullName.split('.').pop() || '';
+
+  if (!(ext in contentTypes)) {
+    throw new Error('getContentType: empty or unknown extension');
+  }
+
+  return contentTypes[ext];
+};
