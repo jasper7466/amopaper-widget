fcc0f83f78a7da8864b6a895a25b64758235b437
Tim <jasper7466@yandex.ru>
2023-02-13 22:23:59 +0500

    API. Base URL. Fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   isLocalTokenServer: true,
   // nopaperBaseUrl: 'https://nopaper-demo.abanking.ru/lk-api/external/api',
   // nopaperBaseUrl: 'https://nopaper-test.abanking.ru/lk-api/external/api',
-  nopaperBaseUrl: 'https://np-demo.abanking.ru/partner-api',
+  nopaperBaseUrl: 'https://np-demo.abanking.ru/partner-api/api/v1/external/api',
   nopaperBaseTokenUrl: 'https://nopaper-test.abanking.ru/lk-api/external/api',
   localBaseTokenUrl: 'http://localhost:5200',
   getAmoBaseUrl: (domain?: string) => '/api/v4',
