df6788720986172d58d0f91c1ccf9aabbd23e6c5
Tim <jasper7466@yandex.ru>
2023-04-17 20:13:50 +0500

    ESLint/Prettier. CodeGuide 6.1: Отступ: 4 пробела

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // CodeGuide 6.1: Отступ: 4 пробела
+    tabWidth: 4,
+    useTabs: false,
+    // "singleQuote": true,
+    // "semi": true,
+    // "bracketSpacing": true,
+    // "arrowParens": "avoid",
+    // "trailingComma": "es5",
+    // bracketSameLine: true,
+    printWidth: 80,
+};
