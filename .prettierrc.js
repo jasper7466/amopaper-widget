155034dc43f5a8d16623b258b91cb43754c25faa
Tim <jasper7466@yandex.ru>
2023-04-18 18:18:04 +0500

    ESLint/Prettier. CodeGuide 7.1 (1,2); CodeGuide 3.4 (2) - Элементы массивов в декораторе модулей должны быть с новой строки (no-inline) - Висящая запятая в массивах декораторов модулей - Висящая запятая в списке аргументов методов/конструкторов

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,37 @@
-module.exports = {
+const plugins = ['./node_modules/prettier-plugin-multiline-arrays'];
+
+const pluginsConfigs = {
+    // CodeGuide 7.1: Элементы массивов в декораторе модулей должны быть с новой строки (no-inline)
+    // (расширено на все массивы в коде)
+    'prettier-plugin-multiline-arrays': {
+        multilineArraysWrapThreshold: 1,
+    },
+};
+
+const prettierNativeConfig = {
     // CodeGuide 6.1: Отступ: 4 пробела
     tabWidth: 4,
     useTabs: false,
-    // "singleQuote": true,
-    // "semi": true,
-    // "bracketSpacing": true,
-    // "arrowParens": "avoid",
-    // "trailingComma": "es5",
-    // bracketSameLine: true,
-    printWidth: 80,
+    singleQuote: true,
+    semi: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    trailingComma: 'all',
+    bracketSameLine: true,
+    printWidth: 90,
+};
+
+const pluginsConfigConcat = () =>
+    Object.values(pluginsConfigs).reduce(
+        (result, current) => ({
+            ...result,
+            ...current,
+        }),
+        {}
+    );
+
+module.exports = {
+    plugins,
+    ...prettierNativeConfig,
+    ...pluginsConfigConcat(),
 };
